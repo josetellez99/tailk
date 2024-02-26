8aa7406bf9fe0ba86af8c4ad0e0fbc57c4eec008
Jose Tellez <jdrgv999@gmail.com>
2024-02-25 22:33:24 -0500

    Deployment bugs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import db from '@/services/db/database'
 import bcrypt from 'bcrypt'
 
-export const authOptions = {
+const authOptions = {
     providers : [
         CredentialsProvider({
             name: 'Credentials',
