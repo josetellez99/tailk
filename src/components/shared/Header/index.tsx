5e33039fa55e69009e4ebf790de0330b538b2cf9
Jose Tellez <jdrgv999@gmail.com>
2024-02-29 19:23:59 -0500

    trying the changes for fixing the situation with the header and content

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -93,7 +93,6 @@ export function NoLoggedHeader () {
     return (
         <>
             <AppBar 
-                position="static"
                 color="secondary"
             >
                 <Toolbar>
